40d4c9ae427f50110c79e2a87ca0cbd6bcf9928e
Jim Barnes <james.barnes@ucf.edu>
2017-05-31 14:09:20 -0400

    Added configuration items

--- tuition-fees.php
+++ tuition-fees.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Tuition and Fees Plugin
+Plugin Name: UCF Tuition and Fees Plugin
 Version: 1.0.0
 Author: UCF Web Communications
 Description: Provides a shortcode for displaying tuition and fees
@@ -11,3 +11,9 @@ if ( ! defined( 'WPINC' ) ) {
 
 define( 'TUITION_FEES__FILE', __FILE__ );
 
+include_once 'includes/tuition-fees-config.php';
+
+// Initiate the plugin settings
+add_action( 'admin_init', array( 'UCF_Tuition_Fees_Config', 'settings_init' ) );
+// Add the options page
+add_action( 'admin_menu', array( 'UCF_Tuition_Fees_Config', 'add_options_page' ) );
