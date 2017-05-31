f7ff47983329cf1e1df95f1f78d8d50f326e0708
Jim Barnes <james.barnes@ucf.edu>
2017-05-31 13:59:06 -0400

    Initial commit

---
+++ tuition-fees.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Tuition and Fees Plugin
+Version: 1.0.0
+Author: UCF Web Communications
+Description: Provides a shortcode for displaying tuition and fees
+*/
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'TUITION_FEES__FILE', __FILE__ );
+
