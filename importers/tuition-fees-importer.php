2084595acab9f8b9070c7e8ec147605edc204c69
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-12-06 11:51:05 -0500

    Plugged in new function into logic

--- importers/tuition-fees-importer.php
+++ importers/tuition-fees-importer.php
@@ -206,10 +206,7 @@ Success %  : {$success_percentage}%
 
 			foreach( $fees as $fee ) {
 				//Make sure this isn't an "Other" fee
-				if ( stripos( $fee->FeeName, '(Per Hour)' ) === false &&
-					 stripos( $fee->FeeName, '(Per Term)' ) === false &&
-					 stripos( $fee->FeeName, '(Annual)' ) === false )
-				{
+				if ( $this->is_required_fee( $fee ) ) {
 					$resident_total += $fee->MaxResidentFee;
 					$non_resident_total += $fee->MaxNonResidentFee;
 				}
